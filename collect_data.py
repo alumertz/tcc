1ab933e8827820d17463d163a2f9f62b8d56d861
Ana Schardosim <ana.schardosim@sap.com>
2025-08-18 20:35:35 -0300

    feat:

--- collect_data.py
+++ collect_data.py
@@ -1,7 +1,12 @@
 import pandas as pd
 import numpy as np
 
-def process_oncokb(file_path):
+ONCOKB_FILE = './data/onkoKB.tsv'
+NCG_FILE = './data/NCG_cancerdrivers_annotation_supporting_evidence.tsv'
+COSMIC_FILE = './data/cosmic.tsv'
+OUTPUT_FILE = 'processed_genes_list.csv'
+
+def process_oncokb(type, file_path = ONCOKB_FILE):
     """
     Carrega e processa a lista de genes do OncoKB.
     Filtra apenas os genes que são oncogenes ou TSGs.
@@ -18,17 +23,22 @@ def process_oncokb(file_path):
             'Is Tumor Suppressor Gene': 'TSG'
         })
 
-        # Filtra para manter apenas os genes que são Oncogene 'Yes' OU TSG 'Yes'
-        df_filtered = df[(df['Oncogene'] == 'Yes') | (df['TSG'] == 'Yes')].copy()
+        if type == 'canonical':
+            # Filtra para manter apenas os genes que são Oncogene 'Yes' OU TSG 'Yes'
+            df_filtered = df[(df['Oncogene'] == 'Yes') | (df['TSG'] == 'Yes')].copy()
+        elif type == 'candidate':
+            # Filtra para manter apenas os genes que são Oncogene 'No' e TSG 'No'
+            df_filtered = df[(df['Oncogene'] == 'No') & (df['TSG'] == 'No')].copy()
 
-        print(f"OncoKB: Encontrados {len(df_filtered)} genes canônicos.")
+
+        print(f"OncoKB: Encontrados {len(df_filtered)} genes {type}.")
         return df_filtered
 
     except FileNotFoundError:
         print(f"Erro: O arquivo OncoKB '{file_path}' não foi encontrado.")
         return pd.DataFrame(columns=['symbol', 'Oncogene', 'TSG'])
 
-def process_ncg(file_path):
+def process_ncg_canonical(type, file_path = NCG_FILE):
     """
     Carrega e processa a lista de genes do Network of Cancer Genes (NCG).
     Usa as colunas 'NCG_oncogene' e 'NCG_tsg' para determinar o papel do gene.
@@ -37,8 +47,12 @@ def process_ncg(file_path):
     try:
         df = pd.read_csv(file_path, sep='\t')
 
-        # Filtra para manter genes que são marcados como oncogene (1.0) ou tsg (1.0)
-        df_filtered = df[(df['NCG_oncogene'] == 1.0) | (df['NCG_tsg'] == 1.0)].copy()
+        if type == 'canonical':
+            # Filtra para manter genes que são marcados como oncogene (1.0) ou tsg (1.0)
+            df_filtered = df[(df['NCG_oncogene'] == 1.0) | (df['NCG_tsg'] == 1.0)].copy()
+        elif type == 'candidate':
+            # Filtra para manter apenas os genes que são Oncogene 'No' e TSG 'No'
+            df_filtered = df[(df['NCG_oncogene'] == 0) & (df['NCG_tsg'] == 0)].copy()
 
         # Converte as colunas de 0/1 para 'No'/'Yes'
         df_filtered['Oncogene'] = np.where(df_filtered['NCG_oncogene'] == 1.0, 'Yes', 'No')
@@ -50,15 +64,14 @@ def process_ncg(file_path):
         # Remove duplicatas, mantendo a primeira ocorrência do símbolo do gene
         df_final = df_final.drop_duplicates(subset=['symbol']).reset_index(drop=True)
 
-        print(f"NCG: Encontrados {len(df_final)} genes canônicos.")
+        print(f"NCG: Encontrados {len(df_final)} {type}.")
         return df_final
 
     except FileNotFoundError:
         print(f"Erro: O arquivo NCG '{file_path}' não foi encontrado.")
         return pd.DataFrame(columns=['symbol', 'Oncogene', 'TSG'])
 
-
-def process_cgc(file_path):
+def process_cgc(tier: int, file_path = COSMIC_FILE):
     """
     Carrega e processa a lista do COSMIC Cancer Gene Census (CGC).
     Filtra por Tier 1 e interpreta a coluna 'Role in Cancer'.
@@ -68,7 +81,7 @@ def process_cgc(file_path):
         df = pd.read_csv(file_path, sep='\t')
 
         # Filtra para manter apenas os genes de 'Tier 1'
-        df_tier1 = df[df['Tier'] == 1].copy()
+        df_tier1 = df[df['Tier'] == tier].copy()
 
         # Converte a coluna 'Role in Cancer' para string para evitar erros e lida com valores nulos
         df_tier1['Role in Cancer'] = df_tier1['Role in Cancer'].astype(str).str.lower()
@@ -83,9 +96,57 @@ def process_cgc(file_path):
         # Remove duplicatas
         df_final = df_final.drop_duplicates(subset=['symbol']).reset_index(drop=True)
 
-        print(f"CGC (Tier 1): Encontrados {len(df_final)} genes canônicos.")
+        print(f"CGC (Tier {tier}): Encontrados {len(df_final)} genes.")
         return df_final
         
     except FileNotFoundError:
         print(f"Erro: O arquivo CGC '{file_path}' não foi encontrado.")
         return pd.DataFrame(columns=['symbol', 'Oncogene', 'TSG'])
+    
+def process_canonical():
+    # Processa cada arquivo de dados
+    oncokb_genes = process_oncokb(type = "canonical", file = ONCOKB_FILE)
+    ncg_genes = process_ncg_canonical(NCG_FILE)
+    cgc_genes = process_cgc(tier = 1, file_path = COSMIC_FILE)
+
+    # Combina as três listas de genes em um único DataFrame
+    print("\nCombinando as listas de genes...")
+    combined_df = pd.concat([oncokb_genes, ncg_genes, cgc_genes], ignore_index=True)
+    print(f"Total de entradas antes da deduplicação: {len(combined_df)}")
+
+    # Remove linhas onde o símbolo do gene é nulo, caso existam
+    combined_df.dropna(subset=['symbol'], inplace=True)
+    
+    # Agrupa por símbolo de gene e consolida as informações de 'Oncogene' e 'TSG'.
+    # Se um gene for 'Yes' em qualquer fonte, ele será 'Yes' na lista final.
+    final_genes = combined_df.groupby('symbol').agg({
+        'Oncogene': lambda x: 'Yes' if 'Yes' in x.values else 'No',
+        'TSG': lambda x: 'Yes' if 'Yes' in x.values else 'No'
+    }).reset_index()
+
+    # Ordena a lista final por símbolo de gene
+    final_genes_sorted = final_genes.sort_values(by='symbol').reset_index(drop=True)
+
+    # Salva o resultado em um arquivo CSV
+    try:
+        final_genes_sorted.to_csv(OUTPUT_FILE, index=False)
+        print(f"\nProcesso concluído com sucesso!")
+        print(f"A lista final com {len(final_genes_sorted)} genes canônicos foi salva em '{OUTPUT_FILE}'.")
+    except Exception as e:
+        print(f"\nOcorreu um erro ao salvar o arquivo final: {e}")
+
+    # Exibe as primeiras linhas do DataFrame final
+    print("\nAmostra da lista final de genes canônicos:")
+    print(final_genes_sorted.head())
+
+
+def process_candidates():
+    """
+    Função para processar candidatos
+    """
+    print("Processando candidatos")
+    cgc_genes = process_cgc(tier = 2, file_path = COSMIC_FILE)
+    oncokb_genes = process_oncokb(type = "candidate", file_path = ONCOKB_FILE)
+    ncg_genes = process_ncg_canonical(type = "candidate", file_path = NCG_FILE)
+
+    pass
--- main.py
+++ main.py
@@ -1,44 +1,8 @@
 import pandas as pd
 
-from collect_data import process_oncokb, process_ncg, process_cgc
+from collect_data import process_canonical, process_candidates
 
-ONCOKB_FILE = './data/onkoKB.tsv'
-NCG_FILE = './data/NCG_cancerdrivers_annotation_supporting_evidence.tsv'
-COSMIC_FILE = './data/cosmic.tsv'
-OUTPUT_FILE = 'processed_genes_list.csv'
 
 if __name__ == "__main__":
-    # Processa cada arquivo de dados
-    oncokb_genes = process_oncokb(ONCOKB_FILE)
-    ncg_genes = process_ncg(NCG_FILE)
-    cgc_genes = process_cgc(COSMIC_FILE)
-
-    # Combina as três listas de genes em um único DataFrame
-    print("\nCombinando as listas de genes...")
-    combined_df = pd.concat([oncokb_genes, ncg_genes, cgc_genes], ignore_index=True)
-    print(f"Total de entradas antes da deduplicação: {len(combined_df)}")
-
-    # Remove linhas onde o símbolo do gene é nulo, caso existam
-    combined_df.dropna(subset=['symbol'], inplace=True)
-    
-    # Agrupa por símbolo de gene e consolida as informações de 'Oncogene' e 'TSG'.
-    # Se um gene for 'Yes' em qualquer fonte, ele será 'Yes' na lista final.
-    final_genes = combined_df.groupby('symbol').agg({
-        'Oncogene': lambda x: 'Yes' if 'Yes' in x.values else 'No',
-        'TSG': lambda x: 'Yes' if 'Yes' in x.values else 'No'
-    }).reset_index()
-
-    # Ordena a lista final por símbolo de gene
-    final_genes_sorted = final_genes.sort_values(by='symbol').reset_index(drop=True)
-
-    # Salva o resultado em um arquivo CSV
-    try:
-        final_genes_sorted.to_csv(OUTPUT_FILE, index=False)
-        print(f"\nProcesso concluído com sucesso!")
-        print(f"A lista final com {len(final_genes_sorted)} genes canônicos foi salva em '{OUTPUT_FILE}'.")
-    except Exception as e:
-        print(f"\nOcorreu um erro ao salvar o arquivo final: {e}")
-
-    # Exibe as primeiras linhas do DataFrame final
-    print("\nAmostra da lista final de genes canônicos:")
-    print(final_genes_sorted.head())
+   #process_canonical()
+   process_candidates()
